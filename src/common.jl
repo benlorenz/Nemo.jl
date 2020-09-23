c311cffeac1999f49f786d57d48cbd4a83789037
Rafael Fourquet <fourquet.rafael@gmail.com>
2020-09-23 20:19:47 +0200

    follow the Random API for FlintPuiseuxSeries via RandomExtensions.make

--- src/common.jl
+++ src/common.jl
@@ -6,6 +6,3 @@
 
 rand(x::Union{AnticNumberField}, v) =
     rand(Random.GLOBAL_RNG, x, v)
-
-rand(x::Union{FlintPuiseuxSeriesRing,FlintPuiseuxSeriesField}, v1, v2, v...) =
-    rand(Random.GLOBAL_RNG, x, v1, v2, v...)
